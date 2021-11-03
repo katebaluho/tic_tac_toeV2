4009153e408cbf788bb967ca075b912df888e677
katebaluho <katebaluho@yandex.by>
2021-11-03 15:28:22 +0300

    v2

--- src/read_argv.py
+++ src/read_argv.py
@@ -33,7 +33,5 @@ def read_terminal():
 
     try:
         TERMINAL_PARAMS['names']  = sys.argv[2:]
-        if not TERMINAL_PARAMS['names']  and (len(TERMINAL_PARAMS['names'] ) == 1 and TERMINAL_PARAMS['mode'].upper() == "USER"):
-            return None
     except IndexError:
         pass
